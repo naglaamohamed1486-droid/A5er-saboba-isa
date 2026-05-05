996fdb54676fa68e8c26a6193562fa8c4d8b39f6
NaglaaMohamed <naglaamohamed1486@gmail.com>
2026-05-06 02:25:06 +0300

    3ayzaa anaaaam

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,13 +3,6 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search_jobs, name='search_jobs'),
+    path('joblist/', views.job_list, name='job_list'),
    
-   
-    path('details/<int:id>/', views.job_details, name='job_details'),
-    path('saved/', views.saved_jobs, name='saved_jobs'),
-    path('applied/', views.applied_jobs, name='applied_jobs'),
-    path('compare/', views.compare_jobs, name='compare_jobs'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-   
-   path('joblist/', views.job_list, name='job_list'),
 ]
