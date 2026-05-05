588329387886db6bc7322d78b093edcabb2f42a8
habibakhaled0 <habibaakhaledq@gmail.com>
2026-05-06 01:44:32 +0300

    etfooo 3aleekooooooooo

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    cover = models.URLField()
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    time = models.CharField(max_length=100)
+
+    tags = models.JSONField()
+    salary = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    exp = models.CharField(max_length=100)
+
+    description = models.TextField()
+    required = models.JSONField()
+    benefit = models.JSONField()
+    gallery = models.JSONField()
+
+    companyLocation = models.CharField(max_length=200)
+    employees = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
