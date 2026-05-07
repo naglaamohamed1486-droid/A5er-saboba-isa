25cd0d09488204a47242053de107eb3c7efe17b6
NaglaaMohamed <naglaamohamed1486@gmail.com>
2026-05-07 05:27:45 +0300

    ///

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,9 +5,14 @@ urlpatterns = [
     path('search/', views.search_jobs, name='search_jobs'),
     path('joblist/', views.job_list, name='job_list'),
     path('add/', views.add_jobs, name='add_jobs')
+
    #naglaa
    #anaaaaaa henaaaaa 
-   #where the hell is my hu
+   #where the hell is my husbund
+   #ya hlaaak ya 3zaaab 
+
+
+
 
 
 
