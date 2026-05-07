e42cfbd41dc44cdf773a81bdd17cbefa8e0c56de
NaglaaMohamed <naglaamohamed1486@gmail.com>
2026-05-07 05:16:49 +0300

    addjob 3la ma tofrag

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('search/', views.search_jobs, name='search_jobs'),
     path('joblist/', views.job_list, name='job_list'),
+    path('add/', views.add_jobs, name='add_jobs')
    
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Job
+from .forms import JobForm
 
 def job_list(request):
     jobs = Job.objects.all()
     return render(request, 'jobs/job_list.html', {'jobs': jobs})
+
 
 def search_jobs(request):
     query = request.GET.get('q')
@@ -14,3 +16,10 @@ def search_jobs(request):
         jobs = Job.objects.all()
 
     return render(request, 'jobs/search.html', {'jobs': jobs})
+
+def add_jobs(request):
+    form = JobForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('dashboard')
+    return render(request, 'jobs/addjob.html', {'form': form})
