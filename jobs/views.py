c16a228b1f02eed3b61baec2609b666f19232fc2
NaglaaMohamed <naglaamohamed1486@gmail.com>
2026-05-07 12:15:52 +0300

    edittttttt 3ayza tnam bardoo

--- jobs/views.py
+++ jobs/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render,redirect
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+import ast
 from .models import Job
 from .forms import JobForm
-
-def job_list(request):
-    jobs = Job.objects.all()
-    return render(request, 'jobs/job_list.html', {'jobs': jobs})
-
 
 def search(request):
     query = request.GET.get('q')
@@ -62,6 +58,33 @@ def delete_job(request, id):
     job = get_object_or_404(Job, id=id, employer=request.user)
     job.delete()
     return redirect('job_list')
+
+def edit_job(request, id):
+    job = get_object_or_404(Job, id=id, employer=request.user)
+
+    if request.method == "POST":
+        job.cover = request.POST.get("cover")
+        job.title = request.POST.get("title")
+        job.company = request.POST.get("company")
+        job.location = request.POST.get("location")
+        job.time = request.POST.get("time")
+        job.salary = request.POST.get("salary")
+        job.type = request.POST.get("type")
+        job.exp = request.POST.get("exp")
+        job.description = request.POST.get("description")
+        job.companyLocation = request.POST.get("companyLocation")
+        job.employees = request.POST.get("employees")
+
+        job.tags = ast.literal_eval(request.POST.get("tags"))
+        job.required = ast.literal_eval(request.POST.get("required"))
+        job.benefit = ast.literal_eval(request.POST.get("benefit"))
+        job.gallery = ast.literal_eval(request.POST.get("gallery"))
+
+        job.save()
+        return redirect("job_list")
+
+    return render(request, "jobs/editjob.html", {"job": job})
+
 
 #habiba
 
